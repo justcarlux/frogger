6c18c4c67096917179989bf62b6d365dd792e19e
JuqnSK <juanandresfg180@gmail.com>
2026-03-03 20:19:20 -0400

    100% Game Completed

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if not pygame.get_init():
     pygame.init()
 
 metadata = (GameMeta()
-            .with_title("Frogger v0.9 Alpha")
+            .with_title("Frogger v1.0")
             .with_description("Juego de rana ")
             .with_release_date("16/12/2025")
             .with_group_number(2)
